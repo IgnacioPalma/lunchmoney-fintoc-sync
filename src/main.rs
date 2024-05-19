7cc8c6b8afb80aad69ff4bdf4de17519b6fd0d12
Agustín Covarrubias <agustin@agucova.dev>
2024-05-19 15:12:14 -0400

    fix: chunks

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,19 @@ use hyper::client::Client;
 use hyper_tls::HttpsConnector;
 use std::time::Duration;
 
+use anyhow::anyhow;
+use anyhow::Result;
+
 mod fintoc;
 mod lunchmoney;
 mod types;
 
 use fintoc::fetch_fintoc_movements;
-use types::fintoc::AccountCredentials;
-use types::HttpsClient;
+use itertools::Itertools;
 use lunchmoney::{get_all_assets, insert_transactions};
+use types::fintoc::AccountCredentials;
+use types::lunchmoney::Transaction;
+use types::HttpsClient;
 
 #[derive(Args)]
 struct ListMovementsArgs {
@@ -22,17 +27,17 @@ struct ListMovementsArgs {
     #[clap(long, value_parser = humantime::parse_duration)]
     end_to: Option<Duration>,
 
-    #[clap(long)]
-    account_id: String,
+    #[clap(long, env)]
+    fintoc_secret_token: String,
 
     #[clap(long)]
-    secret_token: String,
+    fintoc_account_id: String,
 
     #[clap(long)]
-    link_token: String,
+    fintoc_link_token: String,
 }
 
-async fn cmd_list_fintoc_transactions(client: &HttpsClient, args: ListMovementsArgs) {
+async fn cmd_list_fintoc_transactions(client: &HttpsClient, args: ListMovementsArgs) -> Result<()> {
     let end_date: DateTime<Utc> = {
         let mut end_date = Local::now();
 
@@ -47,24 +52,106 @@ async fn cmd_list_fintoc_transactions(client: &HttpsClient, args: ListMovementsA
         (Local::now() - chrono::Duration::from_std(args.start_from).unwrap()).into();
 
     let credentials = AccountCredentials {
-        account_id: args.account_id,
-        secret_token: args.secret_token,
-        link_token: args.link_token,
+        account_id: args.fintoc_account_id,
+        secret_token: args.fintoc_secret_token,
+        link_token: args.fintoc_link_token,
     };
 
-    let movements = fetch_fintoc_movements(client, credentials, start_date, end_date).await.unwrap();
+    let movements = fetch_fintoc_movements(client, credentials, start_date, end_date)
+        .await
+        .unwrap();
 
     for movement in movements {
         println!("{:?}", movement);
     }
+
+    Ok(())
 }
 
-async fn cmd_list_lunch_money_assets(client: &HttpsClient, api_token: String) {
+async fn cmd_list_lunch_money_assets(client: &HttpsClient, api_token: String) -> Result<()> {
     let assets = get_all_assets(client, &api_token).await;
 
     println!("{:#?}", assets);
+
+    Ok(())
 }
 
+#[derive(Args)]
+struct SyncFintocMovementsArgs {
+    #[clap(long, value_parser = humantime::parse_duration, default_value = "30d")]
+    start_from: Duration,
+
+    #[clap(long, value_parser = humantime::parse_duration)]
+    end_to: Option<Duration>,
+
+    #[clap(long, env)]
+    fintoc_secret_token: String,
+
+    #[clap()]
+    fintoc_link_token: String,
+
+    #[clap()]
+    fintoc_account_id: String,
+
+    #[clap(long, env)]
+    lunch_money_api_token: String,
+
+    #[clap()]
+    lunch_money_asset_id: u64,
+}
+
+async fn cmd_sync_fintoc_movements(
+    client: &HttpsClient,
+    args: SyncFintocMovementsArgs,
+) -> Result<()> {
+    let end_date: DateTime<Utc> = {
+        let mut end_date = Local::now();
+
+        if let Some(duration) = args.end_to {
+            end_date = end_date - chrono::Duration::from_std(duration).unwrap();
+        }
+
+        end_date.into()
+    };
+
+    let start_date: DateTime<Utc> =
+        (Local::now() - chrono::Duration::from_std(args.start_from).unwrap()).into();
+
+    let credentials = AccountCredentials {
+        account_id: args.fintoc_account_id,
+        secret_token: args.fintoc_secret_token,
+        link_token: args.fintoc_link_token,
+    };
+
+    let movements = fetch_fintoc_movements(client, credentials, start_date, end_date)
+        .await
+        .unwrap();
+
+    let lunchmoney_transactions = movements
+        .into_iter()
+        .map(|movement| movement.to_lunchmoney_transaction(args.lunch_money_asset_id))
+        .filter_map(Result::ok)
+        .collect::<Vec<Transaction>>();
+
+    let mut synced_transactions: Vec<u64> = Vec::new();
+
+    for transaction_chunk in &lunchmoney_transactions.into_iter().chunks(50) {
+        let ids = insert_transactions(
+            client,
+            &args.lunch_money_api_token,
+            transaction_chunk.collect(),
+        )
+        .await?;
+
+        synced_transactions.extend(ids);
+    }
+    println!(
+        "Synced a total of {} transactions successfully.",
+        synced_transactions.len()
+    );
+
+    Ok(())
+}
 
 // A CLI to sync movements from Fintoc to Lunch Money
 #[derive(Parser)]
@@ -78,13 +165,14 @@ struct Cmd {
 enum Verb {
     ListMovements(ListMovementsArgs),
     ListLunchMoneyAssets {
-        #[clap(long)]
-        api_token: String,
+        #[clap(long, env)]
+        lunch_money_api_token: String,
     },
+    SyncFintocMovements(SyncFintocMovementsArgs),
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     let cmd = Cmd::parse();
 
     let https = HttpsConnector::new();
@@ -92,8 +180,9 @@ async fn main() {
 
     match cmd.verb {
         Verb::ListMovements(args) => cmd_list_fintoc_transactions(&client, args).await,
-        Verb::ListLunchMoneyAssets { api_token } => {
-            cmd_list_lunch_money_assets(&client, api_token).await
-        }
+        Verb::ListLunchMoneyAssets {
+            lunch_money_api_token,
+        } => cmd_list_lunch_money_assets(&client, lunch_money_api_token).await,
+        Verb::SyncFintocMovements(args) => cmd_sync_fintoc_movements(&client, args).await,
     }
 }
